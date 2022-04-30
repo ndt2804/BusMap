d628ad4a3697c56fef23b44406ff3a689ac4f62e
ndt2804 <100899825+ndt2804@users.noreply.github.com>
2022-04-30 20:25:06 +0700

    done

---
+++ src/main/java/com/example/busmap/BusMapApplication.java
@@ -0,0 +1,13 @@
+package com.example.busmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusMapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/busmap/BusMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.busmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
